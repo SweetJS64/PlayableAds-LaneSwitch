8b0d5b1e5a28109c23ad8e7d2f3163da2d4cb370
SweetJS64 <miroran64@gmail.com>
2026-03-08 03:23:20 +0400

    fixed: double movement

--- Assets/Scripts/InputHelper.cs
+++ Assets/Scripts/InputHelper.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public static class InputHelper
 {
+    private static bool _touchWasUsed;
+
     public static bool WasTap(out float screenX)
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (Input.touchCount > 0)
         {
-            screenX = Input.GetTouch(0).position.x;
-            return true;
+            _touchWasUsed = true;
+            var touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                screenX = touch.position.x;
+                return true;
+            }
+            screenX = 0f;
+            return false;
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (!_touchWasUsed && Input.GetMouseButtonDown(0))
         {
             screenX = Input.mousePosition.x;
             return true;
